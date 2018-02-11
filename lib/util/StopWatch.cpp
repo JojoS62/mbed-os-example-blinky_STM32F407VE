da66af9ef8d73d3d37a101b2cfc1fd4153f8faa1
JojoS <sn2010@onlinehome.de>
2018-02-11 17:46:32 +0100

    added MIT license header

--- lib/util/StopWatch.cpp
+++ lib/util/StopWatch.cpp
@@ -1,4 +1,28 @@
 /*
+ MIT License
+
+Copyright (c) 2018 JojoS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*
+ */
+
+/*
  * StopWatch.cpp
  *
  *  Created on: 10.02.2018
@@ -47,7 +71,7 @@ us_timestamp_t StopWatch::stopLapTime ()
 
 us_timestamp_t StopWatch::getLapTime (int n)
 {
-    if ((n >= 0)  && (n < maxLapTimes))
+    if ((actLapTime >= 0)  && (n <= actLapTime))
         return lapTimes[n];
     else
         return 0;
@@ -55,7 +79,7 @@ us_timestamp_t StopWatch::getLapTime (int n)
 
 int64_t StopWatch::getLapTimeDiff (int n)
 {
-    if ((n >= 0)  && (n < maxLapTimes-1)) {
+    if ((actLapTime >= 1)  && (n <= actLapTime-1)) {
         int64_t delta = lapTimes[n+1] - lapTimes[n] - offset;
         return (delta < 0) ? 0 : delta;  // catch -1 µs due to small readout diff
     }
